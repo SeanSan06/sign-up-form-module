9596b0040ec6038a69cea922c477e8220e5d90b8
Sean San <seanksan@gmail.com>
2026-01-10 19:20:15 -0800

    Add password checker to ensure they match

---
+++ index.js
@@ -0,0 +1,16 @@
+const create_account_button = document.getElementById("create-account-button");
+
+create_account_button.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const password = document.getElementById("password").value;
+    const confirmed_password = document.getElementById("confirmed-password").value;
+
+    if(password !== confirmed_password) {
+        alert("Passwords do not match");
+
+        return;
+    }
+
+    console.log("Passwords match!")
+});
